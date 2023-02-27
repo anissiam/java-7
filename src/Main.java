6dbef8bdaf9a96a97a72af65ca5f78c0a89a7f98
anis <anis.siam@hotmail.com>
2023-02-27 09:51:34 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    static int y; // Global
+    static int x;
+    public static void main(String[] args) {
+        System.out.println(x);
+        x = 50;
+        //this line single comment
+       /* System.out.println("Anis");//Hello
+        System.out.print("Siam");
+        System.out.println(10+10);
+        System.out.println("10"+"10");
+        System.out.println(y);*/
+        int y = 50;
+        int x;//Dec
+        x = 50;
+        System.out.println(x);
+
+        x = 203;
+        System.out.println(x);
+
+    }
+
+    public static void aaaaa(){
+        System.out.println(x);
+
+    }
+}
