c8aacda9c6c77503a24f753513d8d6afad3e75e7
anis <anis.siam@hotmail.com>
2023-02-28 16:59:37 +0200

    //28/Feb/2023 -Variables -Data type -Casting -Operators

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,97 @@
+import java.util.stream.StreamSupport;
+
 public class Main {
     static int y; // Global
     static int x;
     public static void main(String[] args) {
+        int ii = 0;
+        System.out.println(ii+=2);
+        System.out.println(ii);
+
+        int x = 50;
+        int y = 20;
+
+        int result = x + y;
+        System.out.println(result);
+
+       System.out.println(result = result/result);
+
+       System.out.println(result);
+
+        int v = x % y;
+        System.out.println(v);
+
+        System.out.println((byte)v);
+        System.out.println(v);
+
+        int i = 0;
+        i = i + 1;  // i +=1 //i++ ,++i
+
+
+        System.out.println(i);
+
+        i = i * 5;//i*=5
+        i = i % 5;//i%=5
+
+
+
+      /*  int x = 5;
+        int y = 3;
+                //5.0/3
+        double z = (double) x / y;
+        System.out.println(z);*/
+
+
+
+
+        /*int x = 10;  //32 bit
+
+        double y = x; //64 bit
+        System.out.println(y);
+        long v =(int) y;    //64 Long = 32 int
+        //long v =(long) y; //Long 64 = long 64
+        System.out.println(x);
+        byte b =(byte) v;
+        System.out.println(b);
+
+
+        System.out.println((float) y);*/
+       /* int x = 10;
         System.out.println(x);
         x = 50;
+        System.out.println(x);
+
+        //byte w = 180;
+
+        double dd = 10.5;
+        System.out.println(dd);
+
+        boolean bb = true;
+        System.out.println(bb);*/
+
+
+        /*System.out.println(x);
+        x = 50;
+        */
         //this line single comment
        /* System.out.println("Anis");//Hello
         System.out.print("Siam");
         System.out.println(10+10);
         System.out.println("10"+"10");
         System.out.println(y);*/
-        int y = 50;
+        /*int y = 50;
         int x;//Dec
         x = 50;
         System.out.println(x);
 
         x = 203;
-        System.out.println(x);
+        System.out.println(x);*/
 
     }
 
     public static void aaaaa(){
-        System.out.println(x);
+        int x;
+        //System.out.println(x);
 
     }
 }
